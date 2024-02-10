9f40b2ff9d01d4fe8daae87a4c48ed5d5412b243
rakshithvedamurthy1003 <rakshithvedamurthy1003@gmail.com>
2024-02-10 15:51:03 -0500

    Deleted Unnecessary Folders

--- Rakshith/src/Class/PredefinedExample.java
+++
@@ -1,9 +0,0 @@
-package Class;
-
-public class PredefinedExample {
-    public static void main(String[] args) {
-        String s= new String("Akhil");
-        String lowerS= s.toUpperCase();
-        System.out.println(lowerS);
-    }
-}
